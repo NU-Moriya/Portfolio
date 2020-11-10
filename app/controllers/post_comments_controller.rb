8ea6e85004aa0bd051d29f19c129a19e4454c287
NU-Moriya <animopara100pre@gmail.com>
2020-11-10 14:32:54 +0000

    [add]post_comment_function

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,27 @@
 class PostCommentsController < ApplicationController
     
   def create
-  end
-  
-  def edit
-  end
-  
-  def update
+    post = Post.find(params[:post_id])
+    @comment = PostComment.new(post_comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = post.id
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
   
   def destroy
+    @post = Post.find(params[:post_id])
+    comment = @post.post_comments.find(params[:id])
+    comment.destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
   end
   
   
