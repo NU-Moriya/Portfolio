d09be7526768481c08e7ada491ed763417919fe7
NU-Moriya <animopara100pre@gmail.com>
2020-11-17 06:53:34 +0000

    [add]Ajax_for bravo,likes_commitments

--- app/controllers/bravos_controller.rb
+++ app/controllers/bravos_controller.rb
@@ -2,18 +2,18 @@ class BravosController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    bravo= current_user.bravos.create(post_id: params[:post_id])
-    bravo.save
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    @bravo= current_user.bravos.create(post_id: @post.id)
+    @bravo.save
   end
   
   def index
   end
   
   def destroy
-    bravo = Bravo.find_by(post_id: params[:post_id], user_id: current_user.id)
-    bravo.destroy
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    @bravo = Bravo.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @bravo.destroy
   end
   
 end
