8567aa64ddefec2547e661f8a0d5161d4f6df0c4
NU-Moriya <animopara100pre@gmail.com>
2020-11-23 05:26:39 +0000

    [modify]admin_invalid

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_user, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -32,4 +33,16 @@ class Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to user_path(user), notice: 'ようこそ、ゲストユーザーさん!!'
   end
+
+
+  def reject_user
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user
+      if @user.valid_password?(params[:user][:password]) && !@user.is_valid
+        flash[:notice] = "退会済みです"
+        redirect_to new_user_session_path
+      end
+    end
+  end
 end
+
