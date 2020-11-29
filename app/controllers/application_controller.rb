a38d3cd68f8a412672f4b6c1b6d66af537dff1a6
NU-Moriya <animopara100pre@gmail.com>
2020-11-29 06:43:44 +0000

    [modify]guest_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :check_guest, only: [:update, :quit, :invalid]
+  
   protected
   
   def after_sign_up_path_for(resource)
@@ -14,5 +15,12 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :account_name])
+  end
+  
+  def check_guest
+    user = User.find_by(account_name: "guest")
+    if current_user == user
+      redirect_to root_path, alert: 'ゲストユーザーの編集・削除はできません。'
+    end
   end
 end
