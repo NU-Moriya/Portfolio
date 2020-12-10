5e193d5f6f5c13295c5247444f04b275fe63766f
NU-Moriya <animopara100pre@gmail.com>
2020-12-10 13:04:54 +0000

    [add]system/users_spec.rb

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Users", type: :system do
       context '新規登録画面に遷移' do
         it '新規登録に成功する' do
           visit new_user_registration_path
-          pp page.html
           fill_in 'user[name]', with: 'テスト'
           fill_in 'user[account_name]', with: 'test'
           fill_in 'user[email]', with: 'test@example.com'
@@ -15,7 +14,17 @@ RSpec.describe "Users", type: :system do
           fill_in 'user[password_confirmation]', with: '000000'
           click_button 'Sign up'
           expect(page).to have_content 'アカウント登録が完了しました。'
+        end
 
+        it '新規登録に失敗する' do
+          visit new_user_registration_path
+          fill_in 'user[name]', with: ''
+          fill_in 'user[account_name]', with: ''
+          fill_in 'user[email]', with: ''
+          fill_in 'user[password]', with: ''
+          fill_in 'user[password_confirmation]', with: ''
+          click_button 'Sign up'
+          expect(page).to have_content 'ユーザー は保存されませんでした。'
         end
       end
     end
