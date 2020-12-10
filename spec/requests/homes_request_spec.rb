4a512251a07cb4f49ce1fa8693bb24f933f98525
NU-Moriya <animopara100pre@gmail.com>
2020-12-10 12:19:38 +0000

    [start]system spec

--- spec/requests/homes_request_spec.rb
+++ spec/requests/homes_request_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-
+  context "topページが正しく表示される" do
+    before do
+        get root_path
+    end
+    
+    it "topページが表示される" do
+      expect(response).to be_success
+    end
+    
+    it "レスポンスが200を返すこと" do
+      expect(response.status).to eq 200
+    end
+  end
 end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  let(:user){FactoryBot.create(:user)}
+  describe "ユーザー認証のテスト" do
+    describe "ユーザー新規登録" do
+      context '新規登録画面に遷移' do
+        it '新規登録に成功する' do
+          visit new_user_registration_path
+          pp page.html
+          fill_in 'user[name]', with: 'テスト'
+          fill_in 'user[account_name]', with: 'test'
+          fill_in 'user[email]', with: 'test@example.com'
+          fill_in 'user[password]', with: '000000'
+          fill_in 'user[password_confirmation]', with: '000000'
+          click_button 'Sign up'
+          expect(page).to have_content 'アカウント登録が完了しました。'
+
+        end
+      end
+    end
+  end
+end
