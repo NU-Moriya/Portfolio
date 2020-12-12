ab36bcf0f3040e9ca135bc1ba6d69ebdf12d81f3
NU-Moriya <animopara100pre@gmail.com>
2020-12-12 09:23:47 +0000

    [add]system spec user mypage

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   let(:user){FactoryBot.create(:user)}
+  let(:user2){FactoryBot.create(:user)}
   describe "ユーザー認証のテスト" do
     describe "ユーザー新規登録" do
       context '新規登録画面に遷移' do
@@ -25,6 +26,69 @@ RSpec.describe "Users", type: :system do
           fill_in 'user[password_confirmation]', with: ''
           click_button 'Sign up'
           expect(page).to have_content 'ユーザー は保存されませんでした。'
+        end
+      end
+    end
+    describe 'ユーザーログイン' do
+      before do
+        visit new_user_session_path
+      end
+      context 'ログイン画面に遷移' do
+        it 'ログインに成功する' do
+          fill_in 'user[email]', with: user.email
+          fill_in 'user[password]', with: user.password
+          click_button 'Log in'
+          expect(page).to have_content 'ログインしました！'
+        end
+
+        it 'ログインに失敗する' do
+          fill_in 'user[email]', with: ''
+          fill_in 'user[password]', with: ''
+          click_button 'Log in'
+          expect(current_path).to eq(new_user_session_path)
+        end
+      end
+    end
+    describe 'ユーザーのテスト' do
+      before do
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'Log in'
+      end
+      describe 'マイページのテスト' do
+        context '表示の確認' do
+          it 'こだわりさん informationと表示される' do
+            expect(page).to have_content 'こだわりさん information'
+          end
+          it '画像が表示される' do
+            expect(page).to have_css('img.avatar')
+          end
+          it '名前が表示される' do
+            expect(page).to have_content(user.name)
+          end
+          it 'アカウントネームが表示される' do
+            expect(page).to have_content(user.account_name)
+          end
+          it '自己紹介が表示される' do
+            expect(page).to have_content(user.introduction)
+          end
+          it '編集リンクが表示される' do
+            visit user_path(user)
+            expect(page).to have_link '', href: edit_user_path(user)
+          end
+          it 'DMリンクが表示される' do
+            visit user_path(user)
+            expect(page).to have_link '', href: connects_path(user)
+          end
+        end
+      end
+      describe '編集のテスト' do
+        context '編集画面へ遷移' do
+          it '遷移できる' do
+            visit edit_user_path(user)
+            expect(current_path).to eq edit_user_path(user)
+          end
         end
       end
     end
