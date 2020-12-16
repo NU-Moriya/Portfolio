7c199f1b46eab092cf171ec9d59c8bf962928d18
NU-Moriya <animopara100pre@gmail.com>
2020-12-16 08:26:55 +0000

    [start]system_commitments

--- spec/factories/commitments.rb
+++ spec/factories/commitments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :commitment do
-    # post_id {1}
+    post_id {1}
     room_name {"テスト部屋"}  
   end
 end
---
+++ spec/system/commitments_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+RSpec.describe "Commitments", type: :system do
+  describe "こだわりルームのテスト" do
+    let(:user){FactoryBot.create(:user)}
+    let!(:commitment){FactoryBot.create(:commitment)}
+    before do
+    	visit new_user_session_path
+    	fill_in 'user[email]', with: user.email
+    	fill_in 'user[password]', with: user.password
+    	click_button 'Log in'
+    end
+    describe "一覧画面のテスト" do
+      before do
+        visit commitments_path
+      end
+      context "表示の確認" do
+        it "Room's keywordと表示される" do
+          expect(page).to have_content "Room's keyword"
+        end
+        it "検索フォームが表示される" do
+          expect(page).to have_field "commitment[room_name]"
+        end
+        it "New Room's nameと表示される" do
+          expect(page).to have_content "New Room's name"
+        end
+        it 'こだわり部屋検索ボタンが表示される' do
+  		  	expect(page).to have_button "Go !!"
+  		  end
+        it "新規こだわりネームのフォームが表示される" do
+          expect(page).to have_field "commitment[room_name]"
+        end
+        it 'createボタンが表示される' do
+		  	  expect(page).to have_button "create !!"
+		    end
+		    it '作成済みのこだわり部屋ネームが表示される' do
+		      expect(page).to have_content(commitment.room_name)
+		    end
+		    it 'こだわり部屋に入るボタンが表示される' do
+  		  	expect(page).to have_link "Enter your .. !!", href: commitment_path(commitment)
+  		  end
+      end
+      context "こだわり部屋作成の確認" do
+        it "新規こだわり部屋作成に成功する" do
+          fill_in "commitment[room_name]", with: commitment.room_name
+          click_button "create !!"
+          expect(page).to have_content "新しいこだわりルームができました!"
+        end
+        it "新規こだわり部屋作成後にこだわり部屋詳細画面へ遷移する" do
+          fill_in "commitment[room_name]", with: commitment.room_name
+          click_button "create !!"
+          visit commitment_path(commitment)
+          expect(current_path).to eq('/commitments/' + commitment.id.to_s)
+        end
+        it "新規こだわり部屋作成に失敗する" do
+          fill_in "commitment[room_name]", with: ""
+          click_button "create !!"
+          expect(page).to have_content '入力項目を確認してください'
+          expect(current_path).to eq('/commitments')
+        end
+      end
+      context "こだわり部屋検索の確認" do
+        it "こだわり部屋検索結果が表示される" do
+          fill_in "commitment[room_name]", with: commitment.room_name
+          click_button "Go !!"
+          expect(page).to have_content "Result Search for"
+        end
+      end
+      context "こだわり部屋動作の確認" do
+        it "こだわり部屋詳細に遷移する" do
+          click_link "Enter your .. !!"
+          visit commitment_path(commitment)
+          expect(page).to have_content "#{commitment.room_name} 's room"
+        end
+      end
+    end
+  end
+end
