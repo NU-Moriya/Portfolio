df642414daa9512348b6191980562c349c90be90
NU-Moriya <animopara100pre@gmail.com>
2020-12-21 11:28:42 +0000

    [continue]posts_spec.rb

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :post do
     user_id {1}
     commitment_id {1}
-    content {"テスト"}
+    content {"テスト投稿"}
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     name {"name_1"}
+    avatar {"Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/no_image.png'))"}
     sequence(:account_name, "account_1")
     sequence(:email, "test@example.com")
     password {"testchan"}
